ca3b14cf029de53a0776f2db154ed92730fc9bed
yazanyousefalhaj@gmail.com <yazan.alhaj@equityzen.com>
2021-10-04 11:04:16 +0300

    bitCounting

--- bitCounting/index.test.js
+++ bitCounting/index.test.js
@@ -8,5 +8,19 @@
  */
 
 const bitCounting = (num) => {
-  // TODO
+  let sum = 0
+  while(num) {
+    sum += num  & 0x1
+    num >>= 1
+  }
+  return sum
 };
+
+describe('Tests', () => {
+  it('test commonCharacters', () => {
+    expect(bitCounting(1234)).toStrictEqual(5);
+    expect(bitCounting(0)).toStrictEqual(0);
+    expect(bitCounting(1024)).toStrictEqual(1);
+    expect(bitCounting(1023)).toStrictEqual(10);
+  });
+});
