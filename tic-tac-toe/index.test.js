6463abc5bd68f32c4322e1764ac7b2c12e749282
yazanyousefalhaj@gmail.com <yazan.alhaj@equityzen.com>
2021-10-10 11:30:37 +0300

    tic-tac-toe

--- tic-tac-toe/index.test.js
+++ tic-tac-toe/index.test.js
@@ -23,3 +23,35 @@ var solvedBoard = [[0,1,1],
                    [2,1,2]];
 ticTacToe(solvedBoard);//should return 1
 */
+
+const solvedBoard = (board) => {
+    for (let raw of board) {
+        if (raw.every(v => v === 1) || raw.every(v => v === 2)) {
+            return 1
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        const col = [board[0][i], board[1][i], board[2][i]]
+        if (col.every(v => v === 1) || col.every(v => v === 2)) {
+            return 1
+        }
+    }
+    if (board[0][0] === 1 && board[1][1] === 1 && board[2][2] === 1 
+        || board[0][0] === 2 && board[1][1] === 2 && board[2][2] === 2) {
+        return 1
+    }
+
+    if (board[2][0] === 1 && board[1][1] === 1 && board[0][2] === 1 
+        || board[2][0] === 2 && board[1][1] === 2 && board[0][2] === 2) {
+        return 1
+    }
+
+    return -1
+}
+
+describe('Tests', () => {
+  it('test solvedBoard', () => {
+    expect(solvedBoard([[0,0,1], [0,1,2], [2,1,0]])).toStrictEqual(-1);
+    expect(solvedBoard([[0,1,1], [0,1,2], [2,1,2]])).toStrictEqual(1);
+  });
+});
