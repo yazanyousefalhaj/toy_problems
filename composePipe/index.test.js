7df3213f2508209cec961276dce62a9749887093
yazanyousefalhaj@gmail.com <yazan.alhaj@equityzen.com>
2021-10-11 15:36:18 +0300

    composePipe

--- composePipe/index.test.js
+++ composePipe/index.test.js
@@ -11,9 +11,9 @@
  * You can view Compose as moving RIGHT to LEFT through its arguments.
  *
  * Compose Example:
-   var greet = function(name){ return 'hello ' + name;}
-   var exclaim = function(statement) { return statement.toUpperCase() + '!';}
-   var welcome = compose(exclaim, greet);
+   let greet = function(name){ return 'hello ' + name;}
+   let exclaim = function(statement) { return statement.toUpperCase() + '!';}
+   let welcome = compose(exclaim, greet);
    welcome('phillip'); // 'hello PHILLIP!'
  *
  * Step 2: Implement the function Pipe:
@@ -25,10 +25,36 @@
  * You can view Pipe as moving LEFT to RIGHT through its arguments.
  *
  * Pipe Example:
-  var add2 = function(number){ return number + 2; }
-  var multiplyBy3 = function(number){ return number * 3; }
-  var addAndMultiply = pipe(add2, multiplyBy3);
+  let add2 = function(number){ return number + 2; }
+  let multiplyBy3 = function(number){ return number * 3; }
+  let addAndMultiply = pipe(add2, multiplyBy3);
   addAndMultiply(5);//should be 21
-  var addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
+  let addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
   addAndMultiplyTwice(5); //should be 63
  */
+
+const compose = (...fns) => (x) => fns.reduceRight((res, fn) => fn(res), x)
+
+const pipe = (...fns) => (x) => fns.reduce((res, fn) => fn(res), x)
+
+
+
+
+describe('Tests', () => {
+  it('test compose', () => {
+    let greet = (name) => 'hello ' + name;
+    let exclaim = (statement) => statement.toUpperCase() + '!';
+    let welcome = compose(exclaim, greet);
+    expect(welcome('phillip')).toStrictEqual('HELLO PHILLIP!');
+  });
+
+  it('test pipe', () => {
+    let add2 = (number) => number + 2;
+    let multiplyBy3 = (number) => number * 3;
+    let addAndMultiply = pipe(add2, multiplyBy3);
+    expect(addAndMultiply(5)).toStrictEqual(21);
+
+    let addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
+    expect(addAndMultiplyTwice(5)).toStrictEqual(63);
+  });
+});
