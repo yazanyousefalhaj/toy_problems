e37397c0ce2f43a65e717e76e2bb7c6b38d684e5
yazanyousefalhaj@gmail.com <yazan.alhaj@equityzen.com>
2021-10-12 11:15:58 +0300

    rotate weeeeeeee

--- rotate/index.test.js
+++ rotate/index.test.js
@@ -24,6 +24,32 @@ rotate(data, 11)    // => [5, 1, 2, 3, 4]
 rotate(data, 12478) // => [3, 4, 5, 1, 2]
 */
 
-const rotate = function(array, steps) {
+const rotate = (array, steps) => [...array.slice((array.length - steps) % array.length), ...array.slice(0, (array.length - steps) % array.length)]
 
-};
+describe("Tests", () => {
+    it("should work on any data type", () => {
+        expect(rotate(['a', 'b', 'c'], 1)).toStrictEqual(['c', 'a', 'b'])
+        expect(rotate([1.0, 2.0, 3.0], 1)).toStrictEqual([3.0, 1.0, 2.0])
+        expect(rotate([true, true, false], 1)).toStrictEqual([false, true, true])
+
+        let data = [1, 2, 3, 4, 5];
+        expect(rotate(data, 1)).toStrictEqual([5, 1, 2, 3, 4])
+        expect(rotate(data, 2)).toStrictEqual([4, 5, 1, 2, 3])
+        expect(rotate(data, 5)).toStrictEqual([1, 2, 3, 4, 5])
+        expect(rotate(data, 0)).toStrictEqual([1, 2, 3, 4, 5])
+        expect(rotate(data, -1)).toStrictEqual([2, 3, 4, 5, 1])
+        expect(rotate(data, -2)).toStrictEqual([3, 4, 5, 1, 2])
+        expect(rotate(data, -5)).toStrictEqual([1, 2, 3, 4, 5])
+
+    });
+
+    it("should work with negative values", () => {
+        let data = [1, 2, 3, 4, 5];
+        expect(rotate(data, 7)).toStrictEqual([4, 5, 1, 2, 3])
+        expect(rotate(data, 11)).toStrictEqual([5, 1, 2, 3, 4])
+        expect(rotate(data, 12478)).toStrictEqual([3, 4, 5, 1, 2])
+    })
+
+
+});
+
