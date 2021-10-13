4b3a4d3df12d488a8fe0db6c1d24c51fd9020bc0
yazanyousefalhaj@gmail.com <yazan.alhaj@equityzen.com>
2021-10-13 18:10:06 +0300

    nextbigger i have javascript please help

--- nextBigger/index.test.js
+++ nextBigger/index.test.js
@@ -13,6 +13,41 @@ nextBigger(111)==-1
 nextBigger(531)==-1
 */
 
-const nextBigger = function(num) {
+function permut(string) {
+  if (string.length < 2) return string; // This is our break condition
 
+  var permutations = []; // This array will hold our permutations
+  for (var i = 0; i < string.length; i++) {
+    var char = string[i];
+
+    // Cause we don't want any duplicates:
+    if (string.indexOf(char) != i)
+      // if char was used already
+      continue; // skip it this time
+
+    var remainingString =
+      string.slice(0, i) + string.slice(i + 1, string.length); //Note: you can concat Strings via '+' in JS
+
+    for (var subPermutation of permut(remainingString))
+      permutations.push(char + subPermutation);
+  }
+  return permutations;
+}
+
+const nextBigger = function (num) {
+  if (num.toString().length < 2) return -1;
+  const x = Math.min(...permut(num.toString()).map((s) => parseInt(s)).filter(n => n > num))
+  return x === num || x === Infinity? -1: x;
 };
+
+describe("Tests", () => {
+  it("test nextBigger", () => {
+    expect(nextBigger(12)).toEqual(21)
+    expect(nextBigger(513)).toEqual(531)
+    expect(nextBigger(2017)).toEqual(2071)
+    expect(nextBigger(9)).toEqual(-1)
+    expect(nextBigger(111)).toEqual(-1)
+    expect(nextBigger(531)).toEqual(-1)
+  });
+});
+
